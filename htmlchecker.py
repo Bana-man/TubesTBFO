9fbc39f76e9ea4e0f877f0cb18f27c10f829f124
Amalia Putri <amaliaputriii2104@gmail.com>
2023-11-08 20:49:45 +0700

    feat(htmlChecker): tag_list

--- htmlchecker.py
+++ htmlchecker.py
@@ -2,4 +2,16 @@ PDA = open("PDA.txt")
 STATE = ((PDA.readline()).rstrip()).split()
 #print(STATE)
 
+tag_list = ["html", "head", "body", "title", "link", "script", "h1", "h2", "h3", "h4", "h5", "h6", "p", 
+            "br", "em", "b", "abbr", "strong","small","hr","div", "a","img","button", "form", "input",
+            "ol", "table","tr", "td","th"]
+
+# Path: htmlchecker.py
+
+def check_tag(tag):
+    if tag in tag_list:
+        return True
+    return False
+
+
 
