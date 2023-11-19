282a281573827897763fc60a923e6d3d13732925
Amalia Putri <amaliaputriii2104@gmail.com>
2023-11-19 21:51:15 +0700

    feat(HTML checker)

--- htmlchecker.py
+++ htmlchecker.py
@@ -1,54 +1,49 @@
+# Function to check if a tag is in the tag_list
 def check_tag(tag):
-    if tag in tag_list:
-        return True
-    return False
+    return tag in tag_list
 
-# LIST TAG HTML
+# List of HTML tags
 tag_list = ["html", "head", "body", "title", "link", "script", "h1", "h2", "h3", "h4", "h5", "h6", "p", 
             "br", "em", "b", "abbr", "strong","small","hr","div", "a","img","button", "form", "input",
             "ol", "table","tr", "td","th"]
 
-# LIST TAG DALAM HTML
+# List to store tags and attributes
 tag_container = []
 
-# HITUNG LINE
+# Count the number of lines
 line_count = 0
 
 # READ FILE HTML
-read_html = open("index.html")
+with open("index.html") as read_html:
+    for line in read_html:
+        word = line.strip()
+        end_index = -1
+        line_count += 1
 
-for line in read_html:
-    word = line.strip()
-    end_index = -1
-    line_count += 1
+        for i in range(len(line.strip())):
+            if word[i] == "<":
+                start_index = i
+                if end_index != -1:
+                    if word[end_index + 1:start_index] != "":
+                        tag_container.append("$")
 
-    for i in range(len(line.strip())):
-        if word[i] == "<":
-            start_index = i
-            if end_index != -1:
-                if word[end_index+1:start_index] != "":
-                    tag_container.append("$")
+            if word[i] == ">":
+                end_index = i
+                tag_content = word[start_index + 1:end_index]
 
-        # 
-        if word[i] == ">":
-            end_index = i
-            tag_container.append(word[start_index+1:end_index])
-            
-    #         if word[-1] == ">":
-    #             word = word[:-1]
-    #         tag_container.append(word[1:])
+                # Process the tag content
+                if tag_content.startswith("/"):
+                    # Handle closing tags
+                    tag_container.append("/" + tag_content[1:])
+                else:
+                    # Handle opening tags
+                    tag_container.append(tag_content)
+                    if " " in tag_content:
+                        # If the tag has attributes, split them correctly
+                        attributes = tag_content.split(" ", 1)[1]
+                        tag_container.extend(attributes.split(' '))
 
-        #     word = word[1:]
-        #     print(word)
-            # if word[-1] == ">":
-            #     word = word[:-1]
-            # if check_tag(word) == False:
-            #     print("Invalid tag: ", word)
-            # else:
-            #     print("Valid tag: ", word)
-            
-read_html.close()
-
-print("Tag container: ")
-print(tag_container, "\n")
-print("Line count: ", line_count)
+print("Tag container:")
+print(tag_container)
+print("Line count:", line_count)
+['html', 'head', 'title', '$', '/title', 'link', 'rel="stylesheet"', 'href=""', '/', 'script', 'src="#src"', '/script', '/head', 'body', 'em', '$', '/em', 'hr /', '/', 'strong', 'p', '$', '/p', 'p', '$', '/p', 'b', 'p', '$', '/p', 'p', '$', '/p', '/b', '/strong', 'h1', 'abbr', 'title="inijudul"', '$', '/abbr', '/h1', 'h2', 'p', '/p', '/h2', 'h3', 'p', '/p', '/h3', 'h4', 'p', '/p', '/h4', 'h5', 'p', '/p', '/h5', 'h6', 'p', '/p', '/h6', 'p', '/p', '/body', '/html']
