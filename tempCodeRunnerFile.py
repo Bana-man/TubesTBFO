feb2822737b31c7afa3087aa0a022c60eea0c4f0
PanjiSri <neiabaraja04@gmail.com>
2023-11-22 13:54:51 +0700

    delete beberapa hal

--- tempCodeRunnerFile.py
+++
@@ -1,1 +0,0 @@
-print(parsingStack("tes"))
--- tes.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-if len(sys.argv) >= 2:
-    input_file = sys.argv[2]
-    print("Nama file input:", input_file)
-else:
-    print("Usage: python main.py pda.txt \"[nama_file].html\"")
