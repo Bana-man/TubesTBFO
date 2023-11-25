e33f6dbfd692dbcaac977625b305cc5d1c3fa823
Amalia Putri <amaliaputriii2104@gmail.com>
2023-11-26 05:10:50 +0700

    fix(main): complete all

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import sys
 from tasukete import *
+import os
 
+# Hitung line code
+arrayOfLineNumber = []
 class bcolors:
     fail = '\033[91m'     # fail bright red
     success = '\033[92m'  # success bright green
     elmt = '\033[33m'     # tags/attributes yellow
     endc = '\033[0m'      # end color
 
-if sys.argv[0] == "main.py" and sys.argv[1] == "pda.txt":
-    file_html = input("Masukkan nama file HTML: ")
-
+def check_tag(file_html):
     # List tags + attributes
     tag_container = []
 
@@ -34,14 +35,18 @@ if sys.argv[0] == "main.py" and sys.argv[1] == "pda.txt":
                     end_index = len(word) - 1
                     in_comment = False
                     tag_container.append("!--")
+                    arrayOfLineNumber.append(line_count)
                     tag_container.append("$")
+                    arrayOfLineNumber.append(line_count)
                     tag_container.append("--")
+                    arrayOfLineNumber.append(line_count)
 
                 if not in_comment:
                     if word[i] == "<":
                         start_index = i
                         if end_index != -1:
                             tag_container.append("$")
+                            arrayOfLineNumber.append(line_count)
 
                     if word[i] == ">":
                         end_index = i
@@ -50,17 +55,32 @@ if sys.argv[0] == "main.py" and sys.argv[1] == "pda.txt":
                         if tag_content.startswith("/"):
                             # Handle closing tags
                             tag_container.append(tag_content[1:])
+                            arrayOfLineNumber.append(line_count)
                         else:
-                            # Handle opening tags
                             tag_name = tag_content.split()[0]
                             tag_container.append(tag_name)
+                            arrayOfLineNumber.append(line_count)
 
                             attributes_list = []
+
                             # Process attributes
                             if " " in tag_content:
                                 attributes = tag_content.split(" ", 1)[1]
                                 attributes_list = attributes.split()
+                                for i in range(len(attributes_list)):
+                                    arrayOfLineNumber.append(line_count)
 
+                            # print(attributes_list)
+                            i = 0
+                            while i < len(attributes_list):
+                                if attributes_list[i].count('"') % 2 != 0:
+                                    attributes_list[i] = attributes_list[i] + attributes_list[i+1]
+                                    attributes_list.pop(i+1)
+                                    arrayOfLineNumber.pop(i+1)
+                                else :
+                                    i += 1
+
+                            # print(attributes_list)
                             for i, attr in enumerate(attributes_list):
                                 if "=" in attr:
                                     attr_name, attr_value = attr.split("=", 1)
@@ -106,73 +126,102 @@ if sys.argv[0] == "main.py" and sys.argv[1] == "pda.txt":
 
                             tag_container.extend(attributes_list)
 
+    # print(tag_container)
     tag_container_filtered = []
-
-    for tags in tag_container:
-        if tags.startswith("<") and tags.endswith(">"):
-            tag_container_filtered.append(tags[1:-1])
-        elif tags.startswith("<") :
-            tag_container_filtered.append(tags[1:])
-        elif tags.startswith("/"):
+    panjang = len(tag_container)
+    
+    for i in range(panjang):
+        if len(tag_container[i]) == 1 and not tag_container[i].startswith("$"):
+            arrayOfLineNumber.pop(i)
+            continue
+        elif tag_container[i].startswith("<") and tag_container[i].endswith(">"):
+            tag_container_filtered.append(tag_container[i][1:-1])
+        elif tag_container[i].startswith("<") :
+            tag_container_filtered.append(tag_container[i][1:])
+        elif tag_container[i].startswith("/"):
             tag_container_filtered.append("/")
-        elif tags.endswith(">"):
-            tag_container_filtered.append(tags[:-1])
+        elif tag_container[i].endswith(">"):
+            tag_container_filtered.append(tag_container[i][:-1])
         else:
-            tag_container_filtered.append(tags)
+            tag_container_filtered.append(tag_container[i])
+    
+    return tag_container_filtered
 
-        pda = open("PDA.txt")
-        state = ((pda.readline()).rstrip()).split()
-        input_simbol = ((pda.readline()).rstrip()).split()
-        stack_simbol = ((pda.readline()).rstrip()).split()
-        start_state = ((pda.readline()).rstrip()).split()
-        start_stack = ((pda.readline()).rstrip()).split()
-        accepting_state =  ((pda.readline()).rstrip()).split()
-        kondisi = ((pda.readline()).rstrip()).split()
-        pda.close()
+if len(sys.argv) == 3:
+    if sys.argv[1] != "PDA.txt":
+        exit(f"{bcolors.fail}Usage: python main.py PDA.txt \"[nama_file].html\"{bcolors.endc}")
+    else:
+        path = os.getcwd() + "\\" + sys.argv[2]
+        if not os.path.isfile(path):
+            print(f"{bcolors.fail}File {sys.argv[2]} tidak ditemukan{bcolors.endc}\n")
+            exit(f"{bcolors.fail}Usage: python main.py PDA.txt \"[nama_file].html\"{bcolors.endc}")
+        else:
+            input_file = sys.argv[2]
+            isiHTML = check_tag(input_file)
 
-    # Read fungsi transisi
-    pda = open("PDA.txt")
-    temp = pda.readlines()
-    pda.close()
-
-    count = 0
-    transition_function = []
-    panjang_baris = len(temp)
-    for i in range( panjang_baris):
-        if i >= 7:        
-            transition_function.append(temp[i].rstrip().split())
-
-    isiHTML = tag_container_filtered
-    print(f"{bcolors.elmt}{isiHTML}{bcolors.endc}")
-
-    state = start_state
-    stack = start_stack
-    accepting = accepting_state
-
-    for masukkan in isiHTML:
-        flag = False
-        for transisi in transition_function:
-            if (listToString(state) == currentState(transisi) and stack[-1] == topFromStack(transisi) and masukkan == alphabetInput(transisi)):
-                state = nextState(transisi)
-                elemen = parsingStack(addToStack(transisi))
-
-                if (elemen[1] != ''):
-                        stack.append(elemen[0])
-                elif (elemen[1] == ''):
-                        if (elemen[0] == 'e'): 
-                            stack.pop()
-        
-                flag = True
-
-        if (flag == False):
-            exit(f"{bcolors.fail}Syntax Error{bcolors.endc}")
+            pda = open("PDA.txt")
+            state = ((pda.readline()).rstrip()).split()
+            input_simbol = ((pda.readline()).rstrip()).split()
+            stack_simbol = ((pda.readline()).rstrip()).split()
+            start_state = ((pda.readline()).rstrip()).split()
+            start_stack = ((pda.readline()).rstrip()).split()
+            accepting_state =  ((pda.readline()).rstrip()).split()
+            kondisi = ((pda.readline()).rstrip()).split()
+            pda.close()
 
 
-    for i in accepting_state:
-        if (state == i):
-            exit(f"{bcolors.success}Accepted{bcolors.endc}")
-            
-    exit(f"{bcolors.fail}Syntax Error{bcolors.endc}")
+            # Read fungsi transisi
+            pda = open("PDA.txt")
+            temp = pda.readlines()
+            pda.close()
 
+            count = 0
+            transition_function = []
+            panjang_baris = len(temp)
+            for i in range( panjang_baris):
+                if i >= 7:        
+                    transition_function.append(temp[i].rstrip().split())
+
+
+            state = start_state
+            stack = start_stack
+            accepting = accepting_state
+            idx = 0
+            for masukkan in isiHTML:
+                flag = False
+                # print(stack)
+                for transisi in transition_function:
+                    if (listToString(state) == currentState(transisi) and stack[-1] == topFromStack(transisi) and masukkan == alphabetInput(transisi)):
+                        state = nextState(transisi)
+                        elemen = parsingStack(addToStack(transisi))
+
+                        if (elemen[1] != ''):
+                                stack.append(elemen[0])
+                        elif (elemen[1] == ''):
+                                if (elemen[0] == 'e'): 
+                                    stack.pop()
+                
+                        flag = True
+                idx += 1
+
+                if (flag == False):
+                    # print(arrayOfLineNumber)
+                    # print(isiHTML)
+                    line_error = arrayOfLineNumber[idx-1]
+                    print(f"{bcolors.fail}Syntax Error\n{bcolors.endc}")
+                    f = open(sys.argv[2], 'r')
+                    for i in range(line_error):
+                        line = f.readline()
+                    exit(f"{bcolors.fail}Terjadi kesalahan ekspresi pada line {line_error} : {line.strip()}{bcolors.endc}")
+
+
+            for i in accepting_state:
+                if (state == i):
+                    exit(f"{bcolors.success}Accepted{bcolors.endc}")
+
+
+            print(f"{bcolors.fail}Syntax Error{bcolors.endc}\n")
+            print(f"{bcolors.fail}Terjadi kesalahan ekspresi pada line {max(arrayOfLineNumber)} : (Tag {stack[-1]} tidak memiliki penutup){bcolors.endc}")
+            exit()
 else:
-    print(f"{bcolors.fail}Usage: python main.py pda.txt{bcolors.endc}")
+    print(f"{bcolors.fail}Usage: python main.py PDA.txt \"[nama_file].html\"{bcolors.endc}")
